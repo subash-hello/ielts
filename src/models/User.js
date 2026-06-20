e51d14c6b03c21d4482f33a62e0aac1f453b2323
subash-hello <subash.bhandari.876490@gmail.com>
2026-06-20 19:01:37 +0545

    Speed up login/register

--- src/models/User.js
+++ src/models/User.js
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
